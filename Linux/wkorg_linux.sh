cec30acadd2611db325d1de0ddd95987c29a3258
Adriel Filipe <78910163+Adriel-Filipe@users.noreply.github.com>
2021-10-16 11:56:28 -0400

    Update wkorg_linux.sh

--- Linux/wkorg_linux.sh
+++ Linux/wkorg_linux.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-filename=$(date +"%d-%B-%Y_%H-%M")
-year=$(date +"%B")
-## wkhtmltopdf http://google.com file.pdf
-mv file.pdf ./$filename
+
+filename=$(date +"%d-%B-%Y_%H-%M-%S")
+year=$(date +"%Y")
+monthname=$(date +"%B")
+
+year_folder=./$year 
+if [ ! -d "$year" ]; then
+echo "criando pastas do ano de $year..."
+mkdir $year janeiro fevereiro marco abril maio junho julho agosto setembro outubro novembro dezembro
+mv janeiro fevereiro marco abril maio junho julho agosto setembro outubro novembro dezembro ./$year
+fi
+
+wkhtmltopdf https://google.com file.pdf
+mv file.pdf $filename.pdf
+mv $filename.pdf ./$year/$monthname
+echo "arquivo gerado e salvo em $year/$monthname."
